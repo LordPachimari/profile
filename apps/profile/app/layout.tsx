64a6045e5b4312fe0202a64fb389e43d6b90def3
LordPachimari <opachimari@gmail.com>
2022-12-23 01:45:17 +1100

    css

--- apps/profile/app/layout.tsx
+++ apps/profile/app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
-
+import "./globals.css";
+import Overview from "./Overview";
+import styles from "./page.module.css";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +13,45 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="wrapper">
+          <div className="page">
+            <div className="layoutParent">
+              <div className="margins">
+                <div className="pageTitleContainer">
+                  <h1>Profile</h1>
+                </div>
+                <div className="profileContainer">
+                  <div className="profile">
+                    <div className="avatarContainer">
+                      <div className="avatar"></div>
+                    </div>
+                    <div className="aboutContainer">
+                      <div className="name"></div>
+                      <div className="about"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="parent">
+                  <div>
+                    <div className="projectTitleContainer">
+                      <h1>Fun projects</h1>
+                    </div>
+                    <Overview />
+
+                    <div className="projectsContainer">
+                      <div className="projects">
+                        <div className="list"></div>
+                        <div className="project">{children}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
